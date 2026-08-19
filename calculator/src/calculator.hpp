af6b1068141f4fedd1ba78c042644b12ec84e970
Gabriel Cardoso de Faria <gacarfaria@gmail.com>
2026-08-19 23:41:42 +0000

    fix: divide function

--- calculator/src/calculator.hpp
+++ calculator/src/calculator.hpp
@@ -13,5 +13,8 @@ class Calculator {
 
   inline T multiply() { return number1 * number2; }
 
-  inline T divide() { return number1 / number2; }
+  inline T divide() {
+      if (number1 == 0 && number2 == 0) return 0;
+      return number1 / number2;
+  }
 };
